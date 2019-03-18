651cef10b0a1c3c5443f3cb184c1848fa822d40d
Matthew Proudman <proudmandandc@gmail.com>
2019-03-18 22:15:00 +0000

    basic cb

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    module: {
+        rules:[
+            test: /\.(js|jsx)$/,
+            exclude:  /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        ]
+    },
+    output: {
+        filename: "static/dist/js/app.js"
+    }
+}
